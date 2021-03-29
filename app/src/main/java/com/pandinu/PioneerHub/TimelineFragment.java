692cd950ed294bf42e8b6686a52689900959b318
dev-lester94 <dev.lester94@gmail.com>
2021-03-29 01:14:32 -0700

    drawerworkingexceptcolorproblem

--- app/src/main/java/com/pandinu/PioneerHub/TimelineFragment.java
+++ app/src/main/java/com/pandinu/PioneerHub/TimelineFragment.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -63,6 +64,9 @@ public class TimelineFragment extends Fragment {
 
         // Tie DrawerLayout events to the ActionBarToggle
         mDrawer.addDrawerListener(drawerToggle);
+        nvDrawer = (NavigationView) v.findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
         //drawerToggle = ((AppCompatActivity)getActivity()).setupDrawerToggle();
 
         // Setup toggle to display hamburger icon with nice animation
@@ -77,6 +81,28 @@ public class TimelineFragment extends Fragment {
         });
 
         return v;
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    private void selectDrawerItem(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.studentFeed:
+                Toast.makeText(getActivity(), "studentFeed", Toast.LENGTH_SHORT).show();
+                mDrawer.closeDrawers();
+                break;
+            default:
+                Toast.makeText(getActivity(), "default", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private ActionBarDrawerToggle setupDrawerToggle() {
