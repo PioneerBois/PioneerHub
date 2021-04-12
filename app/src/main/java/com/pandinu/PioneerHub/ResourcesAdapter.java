513e06bc6722d96a3fa978db09525dd1d00d3e85
timmypass17 <timmypass17@gmail.com>
2021-04-12 12:24:00 -0700

    Adding resource fragment

---
+++ app/src/main/java/com/pandinu/PioneerHub/ResourcesAdapter.java
@@ -0,0 +1,94 @@
+package com.pandinu.PioneerHub;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.pandinu.PioneerHub.Model.Resource;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class ResourcesAdapter extends RecyclerView.Adapter<ResourcesAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Resource> resources;
+
+    public ResourcesAdapter(Context context, List<Resource> resources) {
+        this.context = context;
+        this.resources = resources;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_resource, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Resource resource = resources.get(position);
+        holder.bind(resource);
+    }
+
+    @Override
+    public int getItemCount() {
+        return resources.size();
+    }
+
+    // Recall: vh represent one item/row
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivThumbnail;
+        private TextView tvName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivThumbnail = itemView.findViewById(R.id.ivThumbnail);
+            tvName = itemView.findViewById(R.id.tvName);
+        }
+
+        public void bind(Resource resource) {
+            tvName.setText(resource.getName());
+
+            // We made thumbnail optional, so we check if it exists in the back4app
+            ParseFile image = resource.getThumbnail();
+            if (image != null) {
+                Glide.with(context)
+                        .load(image.getUrl())
+                        .into(ivThumbnail);
+            }
+
+            ivThumbnail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "Clicked!", Toast.LENGTH_SHORT).show();
+                    String url = resource.getWebsiteUrl();
+
+                    Uri webpage = Uri.parse(url);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                    // Have to add context for getPackageManager() and startActivity() because those are methods of Context.
+                    // You can use this method inside an Activity (because an Activity is a Context)
+                    // But if youre calling it elsewhere, you need to pass a Context.
+                    if (intent.resolveActivity(context.getPackageManager()) != null) {
+                        context.startActivity(intent);
+                    } else {
+                        Log.d("ImplicitIntents", "Can't handle this!");
+                    }
+                }
+            });
+        }
+    }
+}
