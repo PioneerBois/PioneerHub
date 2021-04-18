c140e41ab03129b94812313169b09729b4a431cb
Dinesh Pandey <medinu95@gmail.com>
2021-04-18 15:18:07 -0700

    profile fragment

--- app/src/main/java/com/pandinu/PioneerHub/fragments/ProfileFragment.java
+++ app/src/main/java/com/pandinu/PioneerHub/fragments/ProfileFragment.java
@@ -2,10 +2,15 @@ package com.pandinu.PioneerHub.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -14,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
 import com.pandinu.PioneerHub.R;
 
 public class ProfileFragment extends Fragment {
@@ -21,6 +27,12 @@ public class ProfileFragment extends Fragment {
     private ImageView ivCoverImage, ivProfileImage;
     private TextView tvUserFullName, tvUserInfo, tvUserDepartment;
     private RecyclerView rvUserPosts;
+
+    private String currentChannel = "";
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     public static final String ARG_USER_FULLNAME = "USER_FULL_NAME";
     public static final String ARG_USER_INFO = "USER_INFO";
@@ -55,12 +67,33 @@ public class ProfileFragment extends Fragment {
             //mParam1 = getArguments().getString(ARG_PARAM1);
             //mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        currentChannel = "Profile";
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        toolbar.setTitle(currentChannel);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+
+        // Find our drawer view
+        mDrawer = (DrawerLayout) v.findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(drawerToggle);
+        nvDrawer = (NavigationView) v.findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+        nvDrawer.setCheckedItem(R.id.action_profile);
 
         ibEditInfo = (ImageButton)v.findViewById(R.id.ib_edit_info);
         ibEditInfo.setOnClickListener(new View.OnClickListener() {
@@ -106,4 +139,77 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return v;
     }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(getActivity(), mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+
+
+    }
+
+    private void selectDrawerItem(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.studentFeed:
+                currentChannel = getString(R.string.student_feed);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+            case R.id.buyAndSell:
+                currentChannel= getString(R.string.buy_and_sell);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+            case R.id.lostAndFound:
+                currentChannel= getString(R.string.lost_and_found);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+            case R.id.housing:
+                currentChannel= getString(R.string.housing);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+            case R.id.news:
+                currentChannel= getString(R.string.news);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+            case R.id.rideSharing:
+                currentChannel= getString(R.string.ride_sharing);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+            default:
+                currentChannel = getString(R.string.student_feed);
+                Toast.makeText(getActivity(), currentChannel, Toast.LENGTH_SHORT).show();
+                toolbar.setTitle(currentChannel);
+                mDrawer.closeDrawers();
+                //queryPost();
+                break;
+        }
+    }
+
+
 }
